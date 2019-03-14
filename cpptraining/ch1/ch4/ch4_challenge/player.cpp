0d0d8e4f814be3112019b9c4681e205eaea3a92a
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-15 02:23:45 +0900

    problem

--- cpptraining/ch1/ch4/ch4_challenge/player.cpp
+++ cpptraining/ch1/ch4/ch4_challenge/player.cpp
@@ -4,13 +4,18 @@
 
 // TODO : why "string" word doesn't work? even have include string header file.. It has to write only std::string.
 //        I have no idea..
-/*void Player::setName(string name) { 
+
+
+/*  Doesn't work.
+void Player::setName(string name) { 
 	this->name = name; 
 }
 string Player::getName() { 
 	return name;
-}*/
+}
+*/
 
+//  Work.
 void Player::setName(std::string name) {
 	this->name = name;
 }
