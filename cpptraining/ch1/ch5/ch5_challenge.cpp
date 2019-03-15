a4375d42664d355624127fb20eb2a679473c57eb
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-16 02:38:42 +0900

    text to morse, morse to text class

---
+++ cpptraining/ch1/ch5/ch5_challenge.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Morse {
+	string alphabet[26];
+	string digit[10];
+	string slash, question, comma, period, plus, equal;
+public:
+	Morse();
+	void text2Morse(string text, string& morse);
+	bool morse2Text(string morse, string& text);	
+};
+Morse::Morse() {
+
+}
+void Morse::text2Morse(string text, string& morse) {
+
+}
+bool Morse::morse2Text(string morse, string& text) {
+	//temp return.
+	return true;
+}
+
+
+int main() {
+
+	cout << "input english text than it will be changed to morse code." << endl;
+	string text;
+	string morseCode;
+	getline(cin, text, '\n');
+
+	Morse morse;
+	morse.text2Morse(text, morseCode);
+	cout << morseCode << endl;
+
+	cout << endl;
+	cout << "morse code will be changed to english text" << endl;
+	if (morse.morse2Text(morseCode, text)) {
+		cout << morseCode<<endl;
+	}
+	else {
+		cout <<"fail to change text to morse code"<<endl;
+	}
+
+
+
+
+	return 0;
+}
