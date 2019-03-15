acdc90fb1ec199570483053672776abb43a4cd83
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-15 22:41:39 +0900

    make increaseBy() function

---
+++ cpptraining/ch1/ch5/5-3.cpp
@@ -0,0 +1,26 @@
+// call by reference practise.
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int r) { radius = r; }
+	int getRadius() { return radius; }
+	void setRadius(int r) { radius = r; }
+	void show() {cout << "The circle of " << radius <<endl; }
+};
+
+void increaseBy(Circle& a, Circle& b) {
+	int r = a.getRadius() + b.getRadius();
+	a.setRadius(r);
+}
+int main() {
+	Circle x(10), y(5);
+	increaseBy(x, y);
+	x.show();
+
+
+
+	return 0;
+}
