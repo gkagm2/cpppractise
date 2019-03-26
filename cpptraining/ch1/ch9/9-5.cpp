c9302ca9a00374b13b885e25d27540c2898ca940
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-27 03:15:51 +0900

    TODO make classes

---
+++ cpptraining/ch1/ch9/9-5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class AbstractGate { // abstract class
+protected:
+	bool x, y;
+public:
+	void set(bool x, bool y) { this->x = x; this->y = y; }
+	virtual bool operation() = 0;
+};
+
+
+
+int main() {
+	ANDGate and;
+	ORGate or;
+	XORGate xor;
+
+	and.set(true, false);
+	or .set(true, false);
+	xor.set(true, false);
+	cout.setf(ios::boolalpha); // print boolean vaule with string 
+	cout << and.operation() << endl; // AND result : false
+	cout << or.operation() << endl; // OR result : true
+	cout << xor.operation() << endl; // XOR result : true
+	return 0;
+}
