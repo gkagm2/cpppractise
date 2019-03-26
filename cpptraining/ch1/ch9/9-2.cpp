c2580ad9feda5315bc344756db8511ec60f81282
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-26 20:19:25 +0900

    fixed

---
+++ cpptraining/ch1/ch9/9-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Converter {
+protected:
+	double ratio;
+	virtual double convert(double src) = 0;
+	virtual string getSourceString() = 0;
+	virtual string getDestString() = 0;
+public:
+	Converter(double ratio) { this->ratio = ratio; }
+	void run() {
+		double src;
+		cout << "Change " << getSourceString() << "to " << getDestString();
+		cout << "input " << getSourceString() << " >> ";
+		cin >> src;
+		cout << "result : " << convert(src) << getDestString() << endl;
+	}
+};
+class KmToMile : public Converter {
+public:
+	KmToMile(double n) : Converter(n) {
+
+	}
+	double convert(double src) {
+		//TODO
+	}
+	string getSourceString() {
+		//TODO
+	}
+	string getDestString() {
+		//TODO
+	}
+};
+
+int main() {
+	KmToMile toMile(1.609344);
+	toMile.run();
+
+	return 0;
+}
