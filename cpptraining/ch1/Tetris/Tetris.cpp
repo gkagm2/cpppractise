ab28c36019532c484c0f8aa9f941ff110e5da1a7
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-10 07:26:42 +0900

    .

--- cpptraining/ch1/Tetris/Tetris.cpp
+++ cpptraining/ch1/Tetris/Tetris.cpp
@@ -3,12 +3,12 @@
 #include <Windows.h>
 
 #define COL GetStdHandle(STD_OUTPUT_HANDLE) //Std 출력 핸들 (필수)
-#define ORIGINAL SetConsoleTextAttribute(COL, 0x0007); //콘솔 색깔 바꾸기 (0x0007 : 밝은 회색)
-#define GOLD SetConsoleTextAttribute(COL, 0x0006); //금색
-#define RED SetConsoleTextAttribute(COL, 0x000c); //빨강
+#define ORIGINAL SetConsoleTextAttribute(COL, 0x0007) //콘솔 색깔 바꾸기 (0x0007 : 밝은 회색)
+#define GOLD SetConsoleTextAttribute(COL, 0x0006) //금색
+#define RED SetConsoleTextAttribute(COL, 0x000c) //빨강
 
 using namespace std;
 
 Tetris::Tetris(Point reference_pos = Point(1, 1), int key_esc = KEY_ESC, int key_left = KEY_LEFT, int key_rotate = KEY_UP, int key_down = KEY_DOWN, int key_space = KEY_SPACE) {
-
+	running_ = true;
 }
--- cpptraining/ch1/Tetris/Tetris.h
+++ cpptraining/ch1/Tetris/Tetris.h
@@ -3,6 +3,11 @@ using namespace std;
 #include "Point.h"
 #include "Controller.h"
 class Tetris {
+	bool running_;				//종료 조건인 경우 이 값이 true가 됨
+	Point reference_pos_;		//화면상에 나타낼 기준 위치 (1, 1)인 경우 실제 (0, 0)으로 맵핑됨
+	
+
+
 public:
 	//생성자 : 기준 위치, 컨트롤 키 설정 가능
 	Tetris(Point reference_pos = Point(1, 1), int key_esc = KEY_ESC, int key_left = KEY_LEFT, int key_rotate = KEY_UP, int key_down = KEY_DOWN, int key_space = KEY_SPACE);
