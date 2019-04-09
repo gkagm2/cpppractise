5a6cbe6cae48539edc93d62c5133f7d7eb6bea0e
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-10 06:32:55 +0900

    Have to make Tetris class

--- cpptraining/ch1/Tetris/Tetris.h
+++ cpptraining/ch1/Tetris/Tetris.h
@@ -1,8 +1,11 @@
 #pragma once
 using namespace std;
 #include "Point.h"
+#include "Controller.h"
 class Tetris {
 public:
 	//생성자 : 기준 위치, 컨트롤 키 설정 가능
-	//Tetris(Point reference_pos = Point(1, 1),  int key_esc = KEY_ESC, 
+	Tetris(Point reference_pos = Point(1, 1), int key_esc = KEY_ESC, int key_left = KEY_LEFT, int key_rotate = KEY_UP, int key_down = KEY_DOWN, int key_space = KEY_SPACE);
+
+
 };
