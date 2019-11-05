6f688e11f5b370d2ffa18077395e13c77c105df7
HyeonMyeongJang <gkagm2@gmail.com>
2019-11-05 09:34:26 +0900

    크레이지 아케이드 폭탄설치 터트리기 추가

--- cpptraining/ch1/Game/MazeGame.cpp
+++ cpptraining/ch1/Game/MazeGame.cpp
@@ -11,6 +11,7 @@ using namespace std;
 1 : 길
 2 : 시작점
 3 : 도착점
+4 : 폭탄
 */
 
 struct _tagPoint {
@@ -53,11 +54,14 @@ void SetMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT pStartPos, PPOINT pEnd
 	strcpy_s(Maze[19], "11111011100011110113");
 }
 
- void OutputMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT tStartPos, PPOINT pEndPos) {
+ void OutputMaze(char Maze[21][21], PPOINT pPlayerPos) {
 	for (int i = 0; i < 20; ++i) {
 		for (int j = 0; j < 20; ++j) {
 			
-			if (pPlayerPos->x == j && pPlayerPos->y == i) {
+			if (Maze[i][j] == '4') {
+				cout << "※";
+			}
+			else if (pPlayerPos->x == j && pPlayerPos->y == i) {
 				cout << "◇";
 			}
 			else if (Maze[i][j] == '0') {
@@ -69,8 +73,11 @@ void SetMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT pStartPos, PPOINT pEnd
 			else if (Maze[i][j] == '2') {
 				cout << "☆";
 			}
-			else if (pEndPos->x == j && pEndPos->y == i) {
+			else if (Maze[i][j] == '4') {
 				cout << "★";
+			}
+			else if (Maze[i][j] == '3') {
+				cout << "A";
 			}
 		}
 		cout << endl;
@@ -80,7 +87,8 @@ void SetMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT pStartPos, PPOINT pEnd
 void MoveUp(char Maze[21][21], PPOINT pPlayerPos) {
 	if (pPlayerPos->y - 1 >= 0) {
 		// 벽인지 체크한다.
-		if (Maze[pPlayerPos->y - 1][pPlayerPos->x] != '0') {
+		if (Maze[pPlayerPos->y - 1][pPlayerPos->x] != '0' &&
+			Maze[pPlayerPos->y - 1][pPlayerPos->x] != '4') {
 			--pPlayerPos->y;
 		}
 	}
@@ -89,7 +97,8 @@ void MoveUp(char Maze[21][21], PPOINT pPlayerPos) {
 void MoveDown(char Maze[21][21], PPOINT pPlayerPos) {
 	if (pPlayerPos->y + 1 < 20) {
 		// 벽인지 체크한다.
-		if (Maze[pPlayerPos->y + 1][pPlayerPos->x] != '0') {
+		if (Maze[pPlayerPos->y + 1][pPlayerPos->x] != '0' &&
+			Maze[pPlayerPos->y + 1][pPlayerPos->x] != '4') {
 			++pPlayerPos->y;
 		}
 	}
@@ -98,7 +107,8 @@ void MoveDown(char Maze[21][21], PPOINT pPlayerPos) {
 void MoveRight(char Maze[21][21], PPOINT pPlayerPos) {
 	if (pPlayerPos->x + 1 < 20) {
 		// 벽인지 체크한다.
-		if (Maze[pPlayerPos->y][pPlayerPos->x + 1] != '0') {
+		if (Maze[pPlayerPos->y][pPlayerPos->x + 1] != '0' &&
+			Maze[pPlayerPos->y][pPlayerPos->x + 1] != '4') {
 			++pPlayerPos->x;
 		}
 	}
@@ -107,7 +117,8 @@ void MoveRight(char Maze[21][21], PPOINT pPlayerPos) {
 void MoveLeft(char Maze[21][21], PPOINT pPlayerPos) {
 	if (pPlayerPos->x - 1 >= 0) {
 		// 벽인지 체크한다.
-		if (Maze[pPlayerPos->y][pPlayerPos->x - 1] != '0') {
+		if (Maze[pPlayerPos->y][pPlayerPos->x - 1] != '0' &&
+			Maze[pPlayerPos->y][pPlayerPos->x - 1] != '4') {
 			--pPlayerPos->x;
 		}
 	}
@@ -134,29 +145,120 @@ void MovePlayer(char Maze[21][21], PPOINT pPlayerPos, char cInput) {
 	}
 }
 
+// 포인터 변수를 const로 생성하면 가리키는 대상의 값을 변경할 수 없다.
+void CreateBomb(char Maze[21][21], const PPOINT pPlayer, PPOINT pBombArr, int* pBombCount) {
+	if (*pBombCount == 5) {
+		return;
+	}
+
+	// 폭탄을 설치한 위치에 설치하지 못하게 한다.
+	for (int i = 0; i < *pBombCount; ++i) {
+		if (pPlayer->x == pBombArr[i].x && pPlayer->y == pBombArr[i].y) {
+			return;
+		}
+	}
+
+	pBombArr[*pBombCount] = *pPlayer; // 폭탄을 설치한 위치 저장
+	++(*pBombCount);
+
+	Maze[pPlayer->y][pPlayer->x] = '4';
+}
+
+void Fire(char Maze[21][21], PPOINT pPlayer, PPOINT pBombArr, int* pBombCount) {
+	for (int i = 0; i < *pBombCount; ++i) {
+		Maze[pBombArr[i].y][pBombArr[i].x] = '1';
+		// 플레이어가 폭탄에 맞았을때 시작점으로 보낸다.
+		if (pPlayer->x == pBombArr[i].x && pPlayer->y == pBombArr[i].y) {
+			// TODO (Sagacity Jang) : 시작점에다가 넣어주기 tPlayerPos
+			pPlayer->x = 0;
+			pPlayer->y = 0;
+		}
+		
+		if (pBombArr[i].y - 1 >= 0) {
+			if (Maze[pBombArr[i].y - 1][pBombArr[i].x] == '0') {
+				Maze[pBombArr[i].y - 1][pBombArr[i].x] = '1';
+			}
+			// 플레이어가 폭탄에 맞았을때 시작점으로 보낸다.
+			if (pPlayer->x == pBombArr[i].x && pPlayer->y == pBombArr[i].y - 1) {
+				// TODO (Sagacity Jang) : 시작점에다가 넣어주기 tPlayerPos
+				pPlayer->x = 0;
+				pPlayer->y = 0;
+			}
+		}
+		if (pBombArr[i].y + 1 < 20) {
+			if (Maze[pBombArr[i].y + 1][pBombArr[i].x] == '0') {
+				Maze[pBombArr[i].y + 1][pBombArr[i].x] = '1';
+			}
+			// 플레이어가 폭탄에 맞았을때 시작점으로 보낸다.
+			if (pPlayer->x == pBombArr[i].x && pPlayer->y == pBombArr[i].y + 1) {
+				// TODO (Sagacity Jang) : 시작점에다가 넣어주기 tPlayerPos
+				pPlayer->x = 0;
+				pPlayer->y = 0;
+			}
+		}
+		if (pBombArr[i].x - 1 >= 0) {
+			if (Maze[pBombArr[i].y][pBombArr[i].x - 1] == '0') {
+				Maze[pBombArr[i].y][pBombArr[i].x - 1] = '1';
+			}
+			// 플레이어가 폭탄에 맞았을때 시작점으로 보낸다.
+			if (pPlayer->x == pBombArr[i].x - 1 && pPlayer->y == pBombArr[i].y) {
+				// TODO (Sagacity Jang) : 시작점에다가 넣어주기 tPlayerPos
+				pPlayer->x = 0;
+				pPlayer->y = 0;
+			}
+		}
+		if (pBombArr[i].x + 1 < 20) {
+			if (Maze[pBombArr[i].y][pBombArr[i].x + 1] == '0') {
+				Maze[pBombArr[i].y][pBombArr[i].x + 1] = '1';
+			}
+			// 플레이어가 폭탄에 맞았을때 시작점으로 보낸다.
+			if (pPlayer->x == pBombArr[i].x + 1 && pPlayer->y == pBombArr[i].y) {
+				// TODO (Sagacity Jang) : 시작점에다가 넣어주기 tPlayerPos
+				pPlayer->x = 0;
+				pPlayer->y = 0;
+			}
+		}
+	}
+
+	*pBombCount = 0;
+}
+
 int main() {
 	// 20 x 20 미로를 만들어준다.
 	char strMaze[21][21];
 
+	
 	POINT tPlayerPos;
 	POINT tStartPos;
 	POINT tEndPos;
 
+	int iBombCount = 0;
+	int iAddBmob = 0;
+
+	POINT tBombPos[5];
+
+
 	SetMaze(strMaze, &tPlayerPos, &tStartPos, &tEndPos);
 		while (true) {
 		system("cls");
-		OutputMaze(strMaze, &tPlayerPos, &tStartPos, &tEndPos); // 미로를 출력한다.
+		OutputMaze(strMaze, &tPlayerPos); // 미로를 출력한다.
 
 		// 도착시
 		if (tPlayerPos.x == tEndPos.x && tPlayerPos.y == tEndPos.y) {
 			cout << "도착했습니다. " << endl;
 			break;
 		}
-
+		cout << "t : 폭탄설치, u : 폭탄 터트리기" << endl;
 		cout << "w : 위, s : 아래, a : 왼쪽, d : 오른쪽, q : 종료 : ";
 		char cInput = _getch();
 		if (cInput == 'q' || cInput == 'Q') {
 			break;
+		}
+		else if (cInput == 't' || cInput == 'T') {
+			CreateBomb(strMaze, &tPlayerPos, tBombPos, &iBombCount);
+		}
+		else if (cInput == 'u' || cInput == 'U') {
+			Fire(strMaze, &tPlayerPos, tBombPos, &iBombCount);
 		}
 
 		MovePlayer(strMaze, &tPlayerPos, cInput);
