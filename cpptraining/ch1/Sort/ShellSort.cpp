4455ee28c99e2581ab0abe19a7f8c88c896588ce
HyeonMyeongJang <gkagm2@gmail.com>
2019-11-12 01:34:12 +0900

    Shell sort 가독성 증가시킴

--- cpptraining/ch1/Sort/ShellSort.cpp
+++ cpptraining/ch1/Sort/ShellSort.cpp
@@ -7,45 +7,40 @@ using namespace std;
 void IncInsertionSort(int list[], int first, int last, int gap) {
 	int i, j, key;
 
-
-	for (i = first + gap; i <= last; i = i + gap) {
+	for (i = first + gap; i <= last; i += gap) {
 		key = list[i];
-
-		for (j = i - gap; j >= first && list[j] > key; j = j - gap) {
+		for (j = i - gap; j >= first && list[j] > key; j -= gap) {
 			list[j + gap] = list[j];
 		}
 		list[j + gap] = key;
 	}
 }
 
-void ShellSort(int list[], int n) {
+void ShellSort(int list[], int maxSize) {
 	int i, gap;
 
-	for (gap = n / 2; gap > 0; gap = gap / 2)
-	{
+	for (gap = maxSize / 2; gap > 0; gap = gap / 2) {
 		if ((gap % 2) == 0) {
-			gap++;
+			++gap;
 		}
 
 		for (i = 0; i < gap; ++i) {
-			IncInsertionSort(list, i, n - 1, gap);
+			IncInsertionSort(list, i, maxSize - 1, gap);
 		}
 	}
 }
 
 int main() {
-	int i;
-	int n = MAXSIZE;
 	int list[MAXSIZE] = { 10,8,6,20,4,3,22,1,0,15 };
 
-	for (i = 0; i < n; ++i) {
+	for (int i = 0; i < MAXSIZE; ++i) {
 		cout << list[i] << " ";
 	}
 
-	ShellSort(list, n);
+	ShellSort(list, MAXSIZE);
 
 	cout << endl;
-	for (i = 0; i < n; ++i) {
+	for (int i = 0; i < MAXSIZE; ++i) {
 		cout << list[i] << " ";
 	}
 
