685cb30454d7e07dafc31b67dd29113c8f1b0eef
HyeonMyeongJang <gkagm2@gmail.com>
2019-11-14 04:04:31 +0900

    수퍼마리오

---
+++ cpptraining/ch1/Game/SuperMario/Stage.cpp
@@ -0,0 +1,19 @@
+#include "Stage.h"
+
+CStage::CStage()
+{
+}
+
+CStage::~CStage()
+{
+}
+
+bool CStage::Init()
+{
+	return true;
+}
+
+bool CStage::Init(const char * pFileName)
+{
+	return true;
+}
