e4935ad06c158ed250e7161e6f70ac4228b6e60c
HyeonMyeongJang <gkagm2@gmail.com>
2019-11-13 22:18:22 +0900

    클래스로 만든 double linked list

---
+++ cpptraining/ch1/DataStructure/DoubleLinkedList_Class/DoubleLinkedList_Class.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+// 클래스로 double linked list 만들기.
+class CListNode {
+public:
+	CListNode() : m_pNext(NULL), m_pPrev(NULL){
+
+	}
+	~CListNode() {
+
+	}
+public:
+	int m_iData;
+	CListNode* m_pNext;
+	CListNode* m_pPrev;
+};
+
+class CList {
+public:
+	CList() {
+		m_pBegin = new CListNode;
+		m_pEnd = new CListNode;
+
+		m_pBegin->m_pNext = m_pEnd;
+		m_pEnd->m_pPrev = m_pBegin;
+
+		m_iSize = 0;
+	}
+	~CList() {
+		clear();
+		delete m_pBegin;
+		delete m_pEnd;
+	}
+private:
+	CListNode* m_pBegin;
+	CListNode* m_pEnd;
+	int m_iSize;
+	
+public:
+	void push_back(int iData) {
+		CListNode* pNode = new CListNode;
+		pNode->m_iData = iData;
+
+		CListNode* pPrev = m_pEnd->m_pPrev;
+		pPrev->m_pNext = pNode;
+		pNode->m_pPrev = pPrev;
+
+		pNode->m_pNext = m_pEnd;
+		m_pEnd->m_pPrev = pNode;
+
+		++m_iSize;
+	}
+
+	int pop_back() {
+		CListNode* pPrev = m_pEnd->m_pPrev;
+
+		// 노드가 없을 경우
+		if (pPrev == m_pBegin) {
+			return INT_MAX;
+		}
+
+		// 있으면
+		int iData = pPrev->m_iData;
+		CListNode* pPrevP = pPrev->m_pPrev;
+		pPrevP->m_pNext = m_pEnd;
+		m_pEnd->m_pPrev = pPrevP;
+
+		--m_iSize;
+		delete pPrev;
+		return iData;
+
+	}
+
+	bool is_empty() {
+		return m_iSize == 0;
+	}
+
+	void clear() {
+		CListNode* pNode = m_pBegin->m_pNext;
+
+		while (pNode != m_pEnd) {
+			CListNode* pNext = pNode->m_pNext;
+			delete pNode;
+			pNode = pNext;
+		}
+
+		m_pBegin->m_pNext = m_pEnd;
+		m_pEnd->m_pPrev = m_pBegin;
+
+		m_iSize = 0;
+	}
+};
+
+int main() {
+	CList list;
+
+	for (int i = 0; i < 100; ++i) {
+		list.push_back(i);
+	}
+
+	while (!list.is_empty()) {
+		cout << list.pop_back() << endl;
+	}
+
+	return 0;
+}
