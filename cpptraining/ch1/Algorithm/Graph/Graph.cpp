3556339a0b22a017f34eacac6da1814d01ee835a
HyeonMyeongJang <gkagm2@gmail.com>
2020-03-05 00:43:34 +0900

    add input  graph  (use stl)

--- cpptraining/ch1/Algorithm/Graph/Graph.cpp
+++ cpptraining/ch1/Algorithm/Graph/Graph.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int adjacencyMatrix[10][10] = {};
-int vertex, edge;
+// 배열 사용
 
 void DirectedGraph() {
+	int adjacencyMatrix[10][10] = {};
+	int vertex, edge;
+
 	cin >> vertex >> edge;
 
 	for (int i = 0; i < edge; ++i) {
@@ -15,6 +18,9 @@ void DirectedGraph() {
 }
 
 void UnDirectedGraph() {
+	int adjacencyMatrix[10][10] = {};
+	int vertex, edge;
+
 	cin >> vertex >> edge;
 
 	for (int i = 0; i < edge; ++i) {
@@ -22,6 +28,35 @@ void UnDirectedGraph() {
 		cin >> u >> v;
 		adjacencyMatrix[u][v] = 1;
 		adjacencyMatrix[v][u] = 1;
+	}
+}
+
+// STL 사용
+
+void DirectedGraphSTL() {
+	vector<int> adjacency[10];
+	int vertex, edge;
+
+	cin >> vertex >> edge;
+
+	for (int i = 0; i < edge; ++i) {
+		int u, v;
+		cin >> u >> v;
+		adjacency[u].push_back(v);
+	}
+}
+
+void UnDirectedGraphSTL() {
+	vector<int> adjacency[10];
+	int vertex, edge;
+
+	cin >> vertex >> edge;
+
+	for (int i = 0; i < edge; ++i) {
+		int u, v;
+		cin >> u >> v;
+		adjacency[u].push_back(v);
+		adjacency[v].push_back(u);
 	}
 }
 
