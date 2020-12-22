a601b82c149b75d1ea186c40a2607ca824cf128d
HyeonMyeongJang <gkagm2@gmail.com>
2020-12-22 21:05:16 +0900

    Pointer to a function

---
+++ cpptraining/ch1/ProfessionalCpp/PointerToAFuciton.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+void Intro() {
+	cout << "Input a and b\n";
+}
+
+int Plus(int a, int b) {
+	return a + b;
+}
+
+int Minus(int a, int b) {
+	return a - b;
+}
+
+int Mul(int a, int b) {
+	return a * b;
+}
+
+int Div(int a, int b) {
+	if (a == 0 || b == 0)
+		return 0;
+	return a / b;
+}
+
+int main() {
+	void(*ffp)();
+	int(*fp)(int, int); // 함수 포인터 변수 // 
+
+	int select;
+	int a, b;
+	while (true) {
+		system("cls");
+
+		cout << "1.Plus 2.Minus, 3.Mul 4.Div 5.Exit\n";
+		cin >> select;
+
+		ffp = Intro;
+		ffp();
+
+		switch (select) {
+		case 1:
+			fp = Plus;
+			break;
+		case 2:
+			fp = Minus;
+			break;
+		case 3:
+			fp = Mul;
+			break;
+		case 4:
+			fp = Div;
+			break;
+		case 5:
+			return 0;
+		default:
+			continue;
+		}
+
+		cout << "input a : ";
+		cin >> a;
+		cout << "input b : ";
+		cin >> b;
+
+		int value = fp(a, b);
+		cout << "result : " << value << "\n";
+		system("pause");
+	}
+
+	return 0;
+}
