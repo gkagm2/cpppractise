88ca7ed4ac2db907e151ad2877d8fded516c0297
HyeonMyeongJang <gkagm2@gmail.com>
2021-01-07 05:08:51 +0900

    복사 생성자 테스트 코드 추가

--- cpptraining/ch1/Study/CPPForGameDev/UsingFuncInDefaultParameter.cpp
+++ cpptraining/ch1/Study/CPPForGameDev/UsingFuncInDefaultParameter.cpp
@@ -1,12 +1,14 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <memory.h>
+using namespace std;
 
 class CTest {
 public:
 	int buffer[10];
 public:
 	CTest() { memset(buffer, 0, sizeof(buffer)); }
+	CTest(const CTest& copy); // copy constructor
 	void Print();
 };
 
@@ -15,6 +17,11 @@ void CTest::Print() {
 		printf("%d,", buffer[i]);
 	}
 	printf("\n");
+}
+
+// copy constructor
+CTest::CTest(const CTest& copy) {
+	printf("copy constructor\n");
 }
 
 void f(CTest a = CTest()) { // 디폴트 파라미터에 함수 사용 가능
@@ -30,3 +37,14 @@ int main() {
 
 	return 0;
 }
+
+/* 사용할 때 아래와 같이 출력 됨.
+32,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,
+*/
+
+/* 복사 생성자를 이용하게 되면 아래와 같이 출력 됨.
+copy constructor
+-858993460,-858993460,-858993460,-858993460,-858993460,-858993460,16119524,7411696,16119572,7412298,
+0,0,0,0,0,0,0,0,0,0,
+*/
