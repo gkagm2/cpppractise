39a1b02f34df6aa4509075ce74ab1d3cd6ec5896
HyeonMyeongJang <gkagm2@gmail.com>
2021-01-11 19:33:06 +0900

    counter clockwise 90 함수 추가, 복사를 이용한 회전 추가 (CW, CCW)

--- cpptraining/ch1/Algorithm/MatrixRotate/MatrixRotate.cpp
+++ cpptraining/ch1/Algorithm/MatrixRotate/MatrixRotate.cpp
@@ -27,6 +27,64 @@ void Rotate90CW(int n) {
 	}
 }
 
+void Rotate90CWCopy(int n) {
+	int **pMap = new int*[n];
+	for (int i = 0; i < n; ++i)
+		pMap[i] = new int[n];
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			pMap[j][n - i - 1] = mat[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			mat[i][j] = pMap[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+		delete [] pMap[i];
+	delete[] pMap;
+}
+
+
+// Rotate 90 degree CCW
+void Rotate90CCW(int n) {
+	for (int i = 0; i < n ; ++i) {
+		for (int j = i; j < n - 1 - i; ++j) {
+			int top = mat[i][j];
+			mat[i][j] = mat[j][n - 1 - i]; // r -> t
+			mat[j][n-1-i] = mat[n - 1 - i][n - 1 - j]; // b -> r
+			mat[n-1-i][n -1 -j] = mat[n - 1 - j][i]; // l -> b
+			mat[n - 1 - j][i] = top; // t - > l
+		}
+	}
+}
+
+void Rotate90CCWCopy(int n) {
+	int **pMap = new int*[n];
+	for (int i = 0; i < n; ++i)
+		pMap[i] = new int[n];
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			pMap[n - j - 1][i] = mat[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			mat[i][j] = pMap[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+		delete[] pMap[i];
+	delete[] pMap;
+}
+
 int main() {
 	int n = 5;
 
@@ -38,8 +96,16 @@ int main() {
 		}
 	}
 
+	Draw(n);
 	Rotate90CW(n);
-	
+	Draw(n);
+	Rotate90CWCopy(n);
+	Draw(n);
+
+	Rotate90CCWCopy(n);
+	Draw(n);
+	Draw(n);
+	Rotate90CCW(n);
 	Draw(n);
 	return 0;
 }
