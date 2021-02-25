09283d916a0819822c2025199312092751eebd3b
HyeonMyeongJang <gkagm2@gmail.com>
2021-02-26 04:33:13 +0900

    반복자 순회 화면 표시 추가.

--- cpptraining/ch1/ProfessionalCpp/Map_MultiMap_UnorderedMap_Iterator.cpp
+++ cpptraining/ch1/ProfessionalCpp/Map_MultiMap_UnorderedMap_Iterator.cpp
@@ -22,13 +22,17 @@ int main()
 	m.insert(make_pair(0, 2));
 	m.insert(make_pair(0, 3));
 
-	multimap<int, int>::iterator iter, lower, up;
+	multimap<int, int>::iterator lower, up;
 
-	iter = m.begin(); // 0
+	cout << "바이너리 이진 트리의 순회\n";
+	for (auto& iter = cbegin(m); iter != cend(m); ++iter) {
+		cout << iter->first << " " << iter->second << "\n";
+	}
+	cout << "\n";
 
 	// 같은 키를 가진 항목만 순회할 수 있는 반복자  lower_bound(), upper_bound()
-	lower = m.lower_bound(iter->first); // 중복되는 키 값의 시작을 가져옴
-	up = m.upper_bound(iter->first); // 중복되는 키 값의 끝을 가져옴
+	lower = m.lower_bound(0); // 중복되는 키 값의 시작을 가져옴
+	up = m.upper_bound(0); // 중복되는 키 값의 끝을 가져옴
 
 	for (; lower != up; ++lower) { // 사용
 		cout << lower->first << " " << lower->second << "\n";
@@ -53,12 +57,21 @@ int main()
 	um.insert(make_pair(0, 2));
 	um.insert(make_pair(0, 3));
 
+	cout << "\n";
+	cout << "해시맵의 순회\n";
+	for (auto& iter = cbegin(um); iter != cend(um); ++iter) {
+		cout << iter->first << " " << iter->second << "\n";
+	}
+	cout << "\n";
+
 	pair<unordered_multimap<int, int >::iterator, unordered_multimap<int, int>::iterator> pair1 = um.equal_range(0); // 키값을 입력받아 중복되는 키의 시작부터 끝의 반복자를 가져옴. first: begin(), second: end()
 	for (; pair1.first != pair1.second; ++pair1.first) {
 		int key = pair1.first->first;
 		int value = pair1.first->second;
 		cout << key << " " << value << "\n";
 	}
+
+
 
 
 	return 0;
